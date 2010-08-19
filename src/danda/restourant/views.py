82315d1bb9e4acfb65bc640a4bea912d2f26d359
Enrico Carlesso <enrico@ecarlesso.org>
2010-08-19 10:27:13 +0200

    Templates and co

--- src/danda/restourant/views.py
+++ src/danda/restourant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Ristorante, Piatto
+from models import Ristorante, Piatto, Ordine
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     ristoranti = Ristorante.objects.all()
@@ -8,10 +9,26 @@ def index(request):
 
 def about(request):
     return render_to_response('about.html', RequestContext(request))
-def show(request, restourant_id):
+
+def show(request):
+    ristoranti = Ristorante.objects.all()
+    return render_to_response('index.html', RequestContext(request, {'ristoranti': ristoranti}))
+
+def show_restourant(request, restourant_id):
     r = Ristorante.objects.get(id = restourant_id)
     return render_to_response('show.html', RequestContext(request,{'r': r}))
 
 def piatti(request):
+    ristoranti = Ristorante.objects.all()
     plates = Piatto.objects.all()
     return render_to_response('piatti.html', RequestContext(request, {'piatti': plates}))
+
+@login_required
+def ordini(request):
+    ordini = Ordine.objects.filter(user = request.user)
+    return render_to_response('ordini.html', RequestContext(request, {'ordini': ordini}))
+
+@login_required
+def show_ordine(request, id_ordine):
+    ordine = Ordine.objects.get(id = id_ordine)
+    return render_to_response('show_ordine.html', RequestContext(request, {'ordine': ordine}))
